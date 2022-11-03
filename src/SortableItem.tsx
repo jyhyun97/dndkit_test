58fffe1ae2a368c8739a1ef0fe3ccd5fef7b4b21
jeonhyun <jeonhyun@student.42seoul.kr>
2022-11-03 14:53:04 +0900

    특정 Sortable 크기 조절

--- src/SortableItem.tsx
+++ src/SortableItem.tsx
@@ -12,6 +12,14 @@ export function SortableItem(props: any) {
     width: '100px',
     height: '100px',
     background: '#ffff0088',
+    gridColumn: 'auto',
+    gridRow: 'auto',
+  }
+  if (props.id === 3) {
+    style.width = '200px'
+    style.height = '200px'
+    style.gridColumn = '1 / 3'
+    style.gridRow = '1 / 3'
   }
 
   return (
